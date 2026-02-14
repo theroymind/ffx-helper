c9a15e7ea4baa6339a9d6daab42bd4c119312d1b
Eric Roy <theroymind@gmail.com>
2026-02-13 21:16:52 -0500

    Speed up e2e tests with some caching?

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,25 +1,23 @@
 import { defineConfig, devices } from "@playwright/test";
 
+const isCI = !!process.env.CI;
+const baseURL = isCI ? "http://localhost:4173" : "http://localhost:5173";
+
 export default defineConfig({
   testDir: "./e2e",
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  forbidOnly: isCI,
+  retries: isCI ? 1 : 0,
+  workers: isCI ? 2 : undefined,
   reporter: "html",
   use: {
-    baseURL: "http://localhost:5173",
+    baseURL,
     trace: "on-first-retry",
   },
   projects: [
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
-      testIgnore: "e2e/mobile.spec.ts",
-    },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
       testIgnore: "e2e/mobile.spec.ts",
     },
     {
@@ -39,8 +37,8 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "npm run dev",
-    url: "http://localhost:5173",
-    reuseExistingServer: !process.env.CI,
+    command: isCI ? "npm run preview" : "npm run dev",
+    url: baseURL,
+    reuseExistingServer: !isCI,
   },
 });
